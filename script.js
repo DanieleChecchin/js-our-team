2bef312be356075f77cc3e29faeb46c1fe1b5202
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-28 14:53:46 +0200

    MILESTONE 0

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+*/
+
+// Creo un array di oggetti
+const ourTeam = [
+    { fullName: 'Wayne Barnett', role: 'Founder & CEO', image: 'wayne-barnett-founder-ceo.png' },
+    { fullName: 'Angela Caroll', role: 'Chief Editor', image: 'angela-caroll-chief-editor.png' },
+    { fullName: 'Walter Gordon', role: 'Office Manager', image: 'walter-gordon-office-manager.jpg' },
+    { fullName: 'Angela Lopez', role: 'Social Media Manager', image: 'angela-lopez-social-media-manager.jpg' },
+    { fullName: 'Scott Estrada', role: 'Developer', image: 'scott-estrada-developer.jpg' },
+    { fullName: 'Barbara Ramos', role: 'Graphic Designer', image: 'barbara-ramos-graphic-designer.jpg' }
+]
